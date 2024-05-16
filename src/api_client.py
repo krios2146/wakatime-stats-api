e003bd35b7f39e3fb06506f44a18a3163118da16
krios2146 <krios2146@gmail.com>
2024-05-17 00:41:53 +0700

    feat: exclude user to the .env

--- src/api_client.py
+++ src/api_client.py
@@ -17,6 +17,7 @@ _ = load_dotenv()
 def get_last_7_days_data() -> dict[str, dict[str, Any] | list[dict[str, Any]]]:
     base_url: str | None = os.getenv("WAKATIME_BASE_URL")
     api_key: str | None = os.getenv("WAKATIME_API_KEY")
+    user: str | None = os.getenv("WAKATIME_USER")
 
     if api_key is None:
         log.error("WAKATIME_API_KEY couldn't be obtained from the .env file")
@@ -30,6 +31,12 @@ def get_last_7_days_data() -> dict[str, dict[str, Any] | list[dict[str, Any]]]:
             "WAKATIME_BASE_URL is None, aborting API call"
         )
 
+    if user is None:
+        log.error("WAKATIME_USER couldn't be obtained from the .env file")
+        raise WakatimeCredentialsMissingError(
+            "WAKATIME_USER is None, aborting API call"
+        )
+
     api_key_encoded = b64encode(api_key.encode())
 
     headers = {"Authorization": f"Basic {api_key_encoded.decode()}"}
@@ -37,7 +44,7 @@ def get_last_7_days_data() -> dict[str, dict[str, Any] | list[dict[str, Any]]]:
     log.info("Requesting last 7 days data from Wakatime")
 
     response = requests.get(
-        f"{base_url}/users/krios2146/stats/last_7_days", headers=headers
+        f"{base_url}/users/{user}/stats/last_7_days", headers=headers
     )
 
     log.debug(f"Response status code: {response.status_code}")
